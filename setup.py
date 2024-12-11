50c456d5f85bcf14a3276b1d760d9bd04f95e2ac
Letsdial <info@letsdial.com>
2024-12-11 18:00:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='letsdial',  # Name of your package
-    version='1.0.0',  # Version of your package
-    author='Letsdial',  # Your name or the organization's name
-    author_email='info@letsdial.com',  # Your email address
-    description='A Python package for integrating Letsdial communication services.',
-    long_description=open('README.md').read(),  # This will read your README.md for long description
-    long_description_content_type='text/markdown',  # Specify the content type of the long description
-    url='https://github.com/lets-dial/Letsdial.git',  # Link to your GitHub repository
-    packages=find_packages(),  # Automatically find all packages in the project directory
-    classifiers=[  # Additional metadata to categorize your project
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+    name='letsdial',
+    version='0.1.0',  # Update this version based on your release
+    author='Letsdial',
+    author_email='info@letsdial.com',
+    description='A brief description of your letsdial package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # If you're using markdown in the README
+    url='https://github.com/lets-dial/lets-dial',  # Link to your GitHub repository
+    packages=find_packages(),  # Automatically include all packages in the repo
+    install_requires=[
+        # Add any dependencies your package needs
+        'requests',
+        'flask',
+    ],
+    classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
-    install_requires=[  # List your package's dependencies here
-        'requests',  # Example: add requests if your package depends on it
-        'numpy',     # Example: if you're using numpy
-        # Add any other libraries your project needs
-    ],
-    python_requires='>=3.7',  # Specify the minimum Python version required
+    python_requires='>=3.6',
 )
